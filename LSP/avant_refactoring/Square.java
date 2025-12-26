243ae402958c064712ad5e1484beaa0653a34807
lamairie <lemairegilles11@gmail.com>
2025-12-26 10:47:44 +0100

    lsP avant refactoring

---
+++ LSP/avant_refactoring/Square.java
@@ -0,0 +1,16 @@
+package LSP.avant_refactoring;
+
+import LSP.avant_refactoring.Rectangle;
+
+class Square extends Rectangle {
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setWidth(height);
+        super.setHeight(height); }
+}
